dff6144bf612517ea949f7c8e3cd005ffb105871
Srajan Garg <srajan.garg@gmail.com>
2017-10-01 05:54:17 +0530

    vhacd added, testing changes

--- examples/ExtendedTutorials/SimpleBox.cpp
+++ examples/ExtendedTutorials/SimpleBox.cpp
@@ -22,6 +22,11 @@ subject to the following restrictions:
 #include "LinearMath/btAlignedObjectArray.h" 
 #include "../CommonInterfaces/CommonRigidBodyBase.h"
 
+#include "LinearMath/btIDebugDraw.h"
+#include "../../Extras/VHACD/public/VHACD.h"
+#include "../SoftDemo/BunnyMesh.h"
+#include "../SoftDemo/TorusMesh.h"
+
 
 struct SimpleBoxExample : public CommonRigidBodyBase
 {
@@ -32,6 +37,9 @@ struct SimpleBoxExample : public CommonRigidBodyBase
 	virtual ~SimpleBoxExample(){}
 	virtual void initPhysics();
 	virtual void renderScene();
+	virtual void doConvexDecomposition(const btVector3& offset,
+		const btScalar* points, unsigned int stridePoints, unsigned int countPoints,
+		const int* triangles, unsigned int strideTriangles, unsigned int countTriangles);
 	void resetCamera()
 	{
 		float dist = 41;
@@ -41,6 +49,80 @@ struct SimpleBoxExample : public CommonRigidBodyBase
 		m_guiHelper->resetCamera(dist,yaw,pitch,targetPos[0],targetPos[1],targetPos[2]);
 	}
 };
+
+void SimpleBoxExample::doConvexDecomposition(const btVector3& offset,
+	const btScalar* points, unsigned int stridePoints, unsigned int countPoints,
+	const int* triangles, unsigned int strideTriangles, unsigned int countTriangles)
+{
+	VHACD::IVHACD* hacd = VHACD::CreateVHACD();
+	VHACD::IVHACD::Parameters params = VHACD::IVHACD::Parameters();
+	params.m_oclAcceleration = false;
+	bool res = hacd->Compute(points, stridePoints, countPoints,
+		triangles, strideTriangles, countTriangles, params);
+
+	if (res)
+	{
+		// Add the resulting convex shapes to a compound shape
+		btCompoundShape* compoundShape = new btCompoundShape();
+
+		unsigned int nConvexHulls = hacd->GetNConvexHulls();
+		VHACD::IVHACD::ConvexHull convexHull;
+		for (unsigned int ch = 0; ch < nConvexHulls; ch++)
+		{
+			hacd->GetConvexHull(ch, convexHull);
+			unsigned int nDoubles = convexHull.m_nPoints * 3;
+			unsigned int i;
+
+			// Calculate centroid (center of mass)
+			btVector3 centroid(0,0,0);
+			for (i = 0; i < nDoubles; i += 3)
+			{
+				centroid += btVector3(
+					(btScalar)convexHull.m_points[i],
+					(btScalar)convexHull.m_points[i + 1],
+					(btScalar)convexHull.m_points[i + 2]);
+			}
+			centroid /= (btScalar)convexHull.m_nTriangles;
+
+
+			// Create convex shape
+			// Adjust points such that the centroid is at (0,0,0)
+			btConvexHullShape* convexShape = convexShape = new btConvexHullShape();
+			for (i = 0; i < nDoubles; i += 3)
+			{
+				btVector3 point(
+					(btScalar)convexHull.m_points[i],
+					(btScalar)convexHull.m_points[i + 1],
+					(btScalar)convexHull.m_points[i + 2]);
+				point -= centroid;
+				convexShape->addPoint(point, false);
+			}
+			convexShape->recalcLocalAabb();
+			m_collisionShapes.push_back(convexShape);
+
+
+			// Append to the compound shape
+			btTransform transform;
+			transform.setIdentity();
+			transform.setOrigin(centroid);
+			compoundShape->addChildShape(transform, convexShape);
+
+
+			// Also create a separate body for each convex shape
+			// transform.setOrigin(offset + btVector3(3,0,0) + centroid);
+			// createRigidBody(0.1f, transform, convexShape);
+		}
+
+		m_collisionShapes.push_back(compoundShape);
+		btTransform transform;
+		transform.setIdentity();
+		transform.setOrigin(offset + btVector3(-3,0,0));
+		createRigidBody(1.0f, transform, compoundShape);
+	}
+
+	hacd->Clean();
+	hacd->Release();
+}
 
 void SimpleBoxExample::initPhysics()
 {
@@ -53,7 +135,7 @@ void SimpleBoxExample::initPhysics()
 	if (m_dynamicsWorld->getDebugDrawer())
 		m_dynamicsWorld->getDebugDrawer()->setDebugMode(btIDebugDraw::DBG_DrawWireframe+btIDebugDraw::DBG_DrawContactPoints);
 
-	///create a few basic rigid bodies
+	// /create a few basic rigid bodies
 	btBoxShape* groundShape = createBoxShape(btVector3(btScalar(50.),btScalar(50.),btScalar(50.)));
 	m_collisionShapes.push_back(groundShape);
 
@@ -65,34 +147,36 @@ void SimpleBoxExample::initPhysics()
 		createRigidBody(mass,groundTransform,groundShape, btVector4(0,0,1,1));
 	}
 
-
-	{
-		//create a few dynamic rigidbodies
-		// Re-using the same collision is better for memory usage and performance
-        btBoxShape* colShape = createBoxShape(btVector3(1,1,1));
+	doConvexDecomposition(btVector3(0,0.5f,2),
+			&gVerticesBunny[0], 3, BUNNY_NUM_VERTICES,
+			gIndicesBunny[0], 3, BUNNY_NUM_TRIANGLES);
+	// {
+	// 	//create a few dynamic rigidbodies
+	// 	// Re-using the same collision is better for memory usage and performance
+ //        btBoxShape* colShape = createBoxShape(btVector3(1,1,1));
 		 
-		m_collisionShapes.push_back(colShape);
+	// 	m_collisionShapes.push_back(colShape);
 
-		/// Create Dynamic Objects
-		btTransform startTransform;
-		startTransform.setIdentity();
+	// 	/// Create Dynamic Objects
+	// 	btTransform startTransform;
+	// 	startTransform.setIdentity();
 
-		btScalar	mass(1.f);
+	// 	btScalar	mass(1.f);
 
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
+	// 	//rigidbody is dynamic if and only if mass is non zero, otherwise static
+	// 	bool isDynamic = (mass != 0.f);
 
-		btVector3 localInertia(0,0,0);
-		if (isDynamic)
-			colShape->calculateLocalInertia(mass,localInertia);
+	// 	btVector3 localInertia(0,0,0);
+	// 	if (isDynamic)
+	// 		colShape->calculateLocalInertia(mass,localInertia);
 
 
-		startTransform.setOrigin(btVector3(
-								 btScalar(0),
-								 btScalar(20),
-								 btScalar(0)));
-		createRigidBody(mass,startTransform,colShape);		 
-	}
+	// 	startTransform.setOrigin(btVector3(
+	// 							 btScalar(0),
+	// 							 btScalar(20),
+	// 							 btScalar(0)));
+	// 	createRigidBody(mass,startTransform,colShape);		 
+	// }
 
 	m_guiHelper->autogenerateGraphicsObjects(m_dynamicsWorld);
 }
